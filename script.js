07b632348db966eab1a4bed6cd9f53b7275ae75c
jlhelwig <jeff.helwig@gmail.com>
2017-08-18 14:00:23 -0500

    updates script

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-// var btns = document.querySelectorAll('button');
-//     var clipboard = new Clipboard(btns);
-//     clipboard.on('success', function(e) {
-//       alert('email copied to clipboard')
-//         console.log(e);
-//     });
-//     clipboard.on('error', function(e) {
-//         console.log(e);
-//         console.log('error');
-//     });
+
     console.log('this is running');
