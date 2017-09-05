e91f233922a456149d5928a759f23eb5dda2bc3e
jlhelwig <jeff.helwig@gmail.com>
2017-09-05 10:42:43 -0500

    node

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 
-    console.log('this is running');
+console.log('this is running');
+var url = 'https://cdn.optimizely.com/json/12345.json';
+var rp = require('request-promise');
+var options = {uri: url, json: true};
+var optimizely = require('./');
+var optly;
+rp(options).then(function(datafile) { optly = optimizely.createInstance({datafile: datafile}) });
